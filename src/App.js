6e14cae6f159fc39acc75accdc76411387a4aa09
SamchanThunder <samchan.lee@gmail.com>
2023-06-24 17:32:03 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Logo from './Assets/Logo.png';
 import React, { useState } from 'react';
 
 function App() {
-
+  const [homepage, showHome] = useState(true);
+  const [infopage,showInfo] = useState(false);
   const handleButtonClick = (boxName) => {
-    console.log();
+    showHome(false);
+    showInfo(true);
   };
 
   return (
@@ -15,7 +17,7 @@ function App() {
       <Helmet><link href='https://fonts.googleapis.com/css?family=Indie Flower' rel='stylesheet'/></Helmet>
       
       {/* HOME */}
-      <div className='Home'>
+      {homepage && (<div className='Home'>
         <div className="CornerText"><b>From Google's Quick Draw Dataset</b></div>
         <div className="CornerTextTwo"><b>Art <p>Against</p> AI</b></div>
         <div className="CornerTextThree"><b>Can You <p>Draw Better</p> Than AI?</b></div>
@@ -34,14 +36,26 @@ function App() {
             <p>Hard</p>
           </button>
         </div>
-      </div>
+      </div>)}
 
       {/* Second Page */}
-        <div className="secondDiv">
-          Test
-        </div>
+        {infopage && (<div className="secondDiv">
+          <div className="InfoBox"> 
+            <p>*</p>
+            <b><u>Instructions</u></b>
+            <ol><b>
+              <li>In the first round, the player will draw given words in under 60 seconds. The AI needs to guess what the player drew to get a point.</li>
+              <li>In the second round, the AI will draw given words in under 60 seconds. The player needs to guess what the AI drew to get a point.</li>
+              <li>After the two rounds, the points will be compared, and whoever has the most points wins!</li>
+            </b></ol>
+            <button className="startBox">
+              <p>START</p>
+            </button>
+          </div>
+        </div>)}
 
     </div>
+
   );
 }
 
