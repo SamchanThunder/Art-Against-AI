27e3f14342c39433c2bb7b915a86a8e46f845926
SamchanThunder <samchan.lee@gmail.com>
2023-06-23 21:56:40 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
+import Logo from './Assets/Logo.png';
 
 function App() {
+  const handleButtonClick = (boxName) => {
+    // Handle button click event for each box
+    alert(`Button ${boxName} clicked!`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Helmet><link href='https://fonts.googleapis.com/css?family=Indie Flower' rel='stylesheet'/></Helmet>
+      <div className="Home">
+        <div className="CornerText"><b>From Google's <p>Quick Draw</p><p>Dataset</p></b></div>
+        <div className="CornerTextTwo"><b>By <p>Samchan Lee</p></b></div>
+
+        <div className="Logo"><img src={Logo} alt="Logo" /></div>
+
+        <div className="flex-container">
+          <button className="easyBox" onClick={() => handleButtonClick('easyBox')}>
+            <p>Easy</p>
+          </button>
+          <button className="mediumBox" onClick={() => handleButtonClick('mediumBox')}>
+            <p>Medium</p>
+          </button>
+          <button className="hardBox" onClick={() => handleButtonClick('hardBox')}>
+            <p>Hard</p>
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
